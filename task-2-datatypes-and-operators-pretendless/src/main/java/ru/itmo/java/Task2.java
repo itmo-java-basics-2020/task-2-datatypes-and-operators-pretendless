3680f874a79e92fc54bbdcda37321e6edc1b1d69
pretendlesss <42684316+pretendless@users.noreply.github.com>
2020-02-21 21:00:16 +0300

    First attempt
    hope its good

---
+++ task-2-datatypes-and-operators-pretendless/src/main/java/ru/itmo/java/Task2.java
@@ -0,0 +1,92 @@
+package ru.itmo.java;
+
+@SuppressWarnings("all")
+public class Task2 {
+
+    /**
+     * Дано произвольное вещественное число. Выведите его дробную часть
+     * <p>
+     * Например
+     * - дано 17.9
+     * - вывод 0.9
+     */
+    double getFractionalPart(double realNumber) {
+        return realNumber - (int)realNumber;
+    }
+
+    /**
+     * Дан символ, верните его код. Не используйте явное приведение типов и любые библиотечные функции
+     */
+    int charCode(char c) {
+        return (int)c;
+    }
+
+    /**
+     * Даны 2 целых числа. Проверьте равны ли они
+     */
+    boolean testIfIntsEqual(Integer a, Integer b) {
+        return a == b;
+    }
+
+    /**
+     * Дано целое число. Выведите {@code true}, если оно входит в заданный диапазон или {@code false}
+     * в иных случаях
+     *
+     * @param number      заданное число
+     * @param leftBound   правая граница диапазона
+     * @param rightBound  верхняя граница диапазона
+     * @param inclusively входят ли границы в заданный даипазон
+     */
+    boolean numberInRange(Integer number, Integer leftBound, Integer rightBound, Boolean inclusively) {
+        return (number >= leftBound && number <= rightBound && inclusively) ||
+                (number > leftBound && number < rightBound);
+    }
+
+    /**
+     * Даны 3 символа. Определите является ли хотя бы один из них цифрой 1..9
+     */
+    boolean atLeastOneIsDigit(char c1, char c2, char c3) {
+        return (c1 >= '0' && c1 <= '9') ||
+                (c2 >= '0' && c2 <= '9') ||
+                (c3 >= '0' && c3 <= '9');
+    }
+
+    /**
+     * Даны два вещественных числа. Определите равны ли они
+     */
+    boolean areRealNumbersEqual(double a, double b) {
+        return a == b; //your code here
+    }
+
+    /**
+     * Дано трехзначное число 100 <= N <= 999. Найдите сумму его чисел.
+     */
+    int sumOfDigits(int n) {
+        return (n % 10) + (n % 100 / 10) + (n / 100); //your code here
+    }
+
+    /**
+     * Дано натуральное число N <= 10000. Выведите следующее после N четное число. Например: N = 8, Result = 10
+     */
+    int nextEvenNumber(int n) {
+        return n - n % 2 + 2; //your code here
+    }
+
+    /**
+     * Школа №32 решила создать 3 дополнительных физмат класса и укомплектовать кабинеты новыми партами.
+     * За одной партой может сидеть 2 ученика. Кол-во учеников в каждом из классов известно (num1, num2, num3).
+     * Выведите минимальное кол-во парт, которые нужно приобрести школе, при условии что каждый из классов сидит в
+     * своем кабинете
+     */
+    int schoolDesks(int num1, int num2, int num3) {
+        return (num1 + num1 % 2) + (num2 + num2 % 2) + (num3 + num3 % 2); //your code here
+    }
+
+    /**
+     * Дано натуральное число N >= 10. Выведите результат применения XOR к последним 2-м цифрам числа N
+     */
+    int xorDigits(int N) {
+        return 100 - N % 100;
+    }
+
+}
