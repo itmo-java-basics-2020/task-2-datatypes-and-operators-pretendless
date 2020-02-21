4a77eab0be326eac1762513906529c4403d5accd
pretendlesss <42684316+pretendless@users.noreply.github.com>
2020-02-21 21:38:14 +0300

    5th attempt
    used Double.Compare method

--- src/main/java/ru/itmo/java/Task2.java
+++ src/main/java/ru/itmo/java/Task2.java
@@ -56,7 +56,7 @@ public class Task2 {
      * Даны два вещественных числа. Определите равны ли они
      */
     boolean areRealNumbersEqual(double a, double b) {
-        return (double)a == (double)b; //your code here
+        return 0 == Double.compare(a, b); //your code here
     }
 
     /**
